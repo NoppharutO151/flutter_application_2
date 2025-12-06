10805a2816d73c813e0c0572dc3fd607c59e33cf
Noppharut151 <noppharutonlamoon@gmail.com>
2025-12-06 14:53:27 +0700

    Week 1 Homework

--- lib/model/recipe.dart
+++ lib/model/recipe.dart
@@ -5,10 +5,22 @@ class Recipe {
   Recipe(this.imageUrl, this.imgLabel);
   static List<Recipe> samples = 
   [
-   Recipe('assets/images/images01.webp', 'Pinocchio'),
-   Recipe('assets/images/images02.webp', 'Sophia Monad'),
-   Recipe('assets/images/images03.webp', 'Lea Florence Monad'),
-   Recipe('assets/images/images04.webp','Black Rabbit Brotherhood'),
+
+    Recipe('assets/images/images01.webp', 'Pinocchio'),
+    Recipe('assets/images/images02.webp', 'Sophia Monad'),
+    Recipe('assets/images/images03.webp', 'Lea Florence Monad'),
+    Recipe('assets/images/images04.webp','Black Rabbit Brotherhood'),
+    Recipe('assets/images/images05.webp', 'Eldest of The Black Rabbit Brotherhood'),
+    Recipe('assets/images/images06.webp', 'Antonia Cerasani'),
+    Recipe('assets/images/images07.webp', 'Belle'),
+    Recipe('assets/images/images08.webp', 'Black Cat'),
+    Recipe('assets/images/images09.webp', 'Laxasia The Complete'),
+    Recipe('assets/images/images10.webp', 'Simon Manus'),
+    Recipe('assets/images/images11.webp', 'Champion Victor'),
+    Recipe('assets/images/images12.webp', 'Fallen Archbishop Andreus'),
+    Recipe('assets/images/images13.webp', 'Giuseppe Geppetto'),
+    Recipe('assets/images/images14.webp', 'King of Riddles, Arlecchino'),
+    Recipe('assets/images/images15.webp', 'Dorothy'),
   ];
 
  }
